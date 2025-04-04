b254b11957a061127fb27986ab81a471e3305e54
JoschD <26184899+JoschD@users.noreply.github.com>
2025-04-04 19:02:47 +0200

    pyside docmock

--- doc/conf.py
+++ doc/conf.py
@@ -227,7 +227,7 @@ texinfo_documents = [
 
 # Add here all modules to be mocked up. When the dependencies are not met
 # at building time. Here used to have PyQT mocked.
-autodoc_mock_imports = ["accwidgets", "qtpy", "pyqtgraph"]
+autodoc_mock_imports = ["accwidgets", "qtpy", "pyqtgraph", "PySide2"]
 
 # -- Type Aliases --------------------------------------------------------------
 
