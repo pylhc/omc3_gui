d23ed36108764bb0a2909ce9c97dbaac758be620
JoschD <26184899+JoschD@users.noreply.github.com>
2025-04-04 18:59:34 +0200

    pyqtgraph docmock

--- doc/conf.py
+++ doc/conf.py
@@ -227,7 +227,7 @@ texinfo_documents = [
 
 # Add here all modules to be mocked up. When the dependencies are not met
 # at building time. Here used to have PyQT mocked.
-autodoc_mock_imports = ["accwidgets", "qtpy"]
+autodoc_mock_imports = ["accwidgets", "qtpy", "pyqtgraph"]
 
 # -- Type Aliases --------------------------------------------------------------
 
